36e5194be78eb5342a3a56be982ddbccc207fd5a
NicolasIannello <nicolas.iannello99@gmail.com>
2024-07-29 16:16:18 -0300

    s

--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  maps:string="https://www.google.com/maps/place/Triunvirato+2219,+B1611CHS+Don+Torcuato,+Provincia+de+Buenos+Aires/@-34.4956987,-58.6107253,15.75z/data=!4m5!3m4!1s0x95bcbb2532621b1d:0x3c6879dafb4b2ebe!8m2!3d-34.4951766!4d-58.6116785?entry=ttu";
 
+  constructor(private router: Router){ }
+
+  mapsG(){
+    window.open(this.maps);
+  }
 }
