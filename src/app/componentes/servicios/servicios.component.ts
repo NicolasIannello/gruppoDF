1313cf3d17fa37fb17e96e42e3f32268a6639514
NicolasIannello <nicolas.iannello99@gmail.com>
2024-08-06 21:38:03 -0300

    textos

--- src/app/componentes/servicios/servicios.component.ts
+++ src/app/componentes/servicios/servicios.component.ts
@@ -14,12 +14,20 @@ export class ServiciosComponent {
   
   cards:Array<any>=[
     { text:"COMPRA DE AUTOS SINIESTRADOS",
-      img:"fa-car-burst"},
+      img:"fa-car-burst",
+      desc:"Gestión de cotizaciones y compras de vehículos siniestrados en todo el país para las mejores compañías de seguros."
+    },
     { text:"SUBASTAS Y LICITACIONES WEB",
-      img:"fa-globe"},
+      img:"fa-globe",
+      desc:"Servicio exclusivo para clientes vendedores Aseguradoras y/o Empresas con flota propia. Gestión de ventas de vehículos mediante licitaciones o subastas Web."
+    },
     { text:"BAJAS CON RECUPERO DE PIEZAS",
-      img:"fa-screwdriver-wrench"}, 
+      img:"fa-screwdriver-wrench",
+      desc:"Servicio exclusivo para compañías de Seguros. Nos encargamos del tratamiento y la gestión completa de sus vehículos con Destrucción Total para recupero de Piezas según Ley 25.761."
+    }, 
     { text:"GESTION DE SCRAP",
-      img:"fa-gear"}
+      img:"fa-gear",
+      desc:"Exclusivo para compañías de Seguros. Retiramos y gestionamos el Scrap de las bajas totales."
+    }
   ]
 }
